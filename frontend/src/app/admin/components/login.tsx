31df24f40439ad49058b33e8f19a91982e4168b3
Ben <ben.arteii@proton.me>
2024-05-18 04:16:27 +0200

    added login logic

--- frontend/src/app/admin/components/login.tsx
+++ frontend/src/app/admin/components/login.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 const AdminLogin: React.FC = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignIn = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+
+        const requestUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}/superuser`;
+
+
+        console.log("called handle sign in");
+        console.log(requestUrl);
+
+        // Send GET request to backend with headers for username and password
+        try {
+            const response = await fetch(requestUrl, {
+                method: 'GET',
+                headers: {
+                    'Username': username,
+                    'Password': password
+                }
+            });
+
+            if (response.ok) {
+                // Handle successful authentication
+                console.log('Authentication successful');
+            } else {
+                // Handle authentication failure
+                console.error('Authentication failed');
+            }
+        } catch (error) {
+            console.error('Error during authentication:', error);
+        }
+    };
+
     return (
         <div className="fixed inset-0 flex justify-center items-center backdrop-blur-md">
             <div className="absolute inset-0 bg-gray-900 bg-opacity-80"></div>
@@ -13,19 +50,21 @@ const AdminLogin: React.FC = () => {
                         This is the management page for Superusers
                     </p>
                     <div className="lg:max-w-sm mx-auto">
-                        <form className="mt-8 space-y-6" action="#" method="POST">
+                        <form className="mt-8 space-y-6" onSubmit={handleSignIn}>
                             <div className="rounded-md shadow-sm -space-y-px">
                                 <div>
-                                    <label htmlFor="email-address" className="sr-only">
-                                        Email address
+                                    <label htmlFor="username" className="sr-only">
+                                        Username
                                     </label>
                                     <input
                                         id="username"
                                         name="username"
-                                        type="username"
+                                        type="text"
                                         autoComplete="username"
                                         required
-                                        className="appearance-none bg-slate-800 rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                        value={username}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                        className="appearance-none bg-slate-800 rounded-none relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-300 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                         placeholder="Username"
                                     />
                                 </div>
@@ -39,7 +78,9 @@ const AdminLogin: React.FC = () => {
                                         type="password"
                                         autoComplete="current-password"
                                         required
-                                        className="appearance-none rounded-none bg-slate-800 relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        className="appearance-none rounded-none bg-slate-800 relative block w-full px-3 py-2 border border-gray-700 placeholder-gray-500 text-gray-300 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                         placeholder="Password"
                                     />
                                 </div>
